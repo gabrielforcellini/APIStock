f366de3f52bd3cbf2d750a2dfe04f2cfda37e2a8
Gabriel <gabriel.forcellini@hotmail.com>
2023-05-20 10:03:08 -0300

    alterado config para dotenv

--- MyProject/src/data-source.ts
+++ MyProject/src/data-source.ts
@@ -1,14 +1,25 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+    username,
+    password,
+    database,
+    host,
+    port
+} = process.env;
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: host,
+    port: parseInt(port),
+    username: username,
+    password: password,
+    database: database,
     synchronize: true,
     logging: false,
     entities: [User],
