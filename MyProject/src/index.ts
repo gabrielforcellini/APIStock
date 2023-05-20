b9c0b3f8643429a66df387c399feaac2da8a87a0
Gabriel <gabriel.forcellini@hotmail.com>
2023-05-20 10:33:35 -0300

    corrigindo index.ts

--- MyProject/src/index.ts
+++ MyProject/src/index.ts
@@ -5,9 +5,8 @@ AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
     const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
+    user.nome = "Timber"
+    user.sobrenome = "Saw"
     await AppDataSource.manager.save(user)
     console.log("Saved a new user with id: " + user.id)
 
