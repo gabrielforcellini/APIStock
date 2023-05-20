f5ab133163f65dc9ccce81ef0fdcda91c152951d
Gabriel <gabrielpavan2014@outlook.com>
2023-05-20 12:10:49 -0300

    fix entitys

--- MyProject/src/entity/Product.ts
+++ MyProject/src/entity/Product.ts
@@ -8,45 +8,45 @@ export class Product {
     @Column({ type: "character varying", length: 50})
     nome!: string
 
-    @Column({type: "text"})
+    @Column({type: "text", nullable: true})
     descricao?: string
 
-    @Column({ type: "character varying", length:50})
+    @Column({ type: "character varying", length:50, nullable: true})
     categoria?: string
 
-    @Column({ type: "numeric", length:10, scale: 2})
+    @Column({ type: "numeric", length:10, scale: 2, nullable: true})
     preco_compra?: number
 
-    @Column({ type: "numeric", length:10, scale: 2})
+    @Column({ type: "numeric", length:10, scale: 2, nullable: true})
     preco_venda?: number
 
-    @Column({ type: "integer"})
+    @Column({ type: "integer", nullable: true})
     quantidade_estoque?: number
 
-    @Column({ type: "character varying", length: 20})
+    @Column({ type: "character varying", length: 20, nullable: true})
     unidade_medida?: string
 
-    @Column({ type: "integer"})
+    @Column({ type: "integer", nullable: true})
     fornecedor_id?: number
 
-    @Column({ type: "character varying", length: 50})
+    @Column({ type: "character varying", length: 50, nullable: true})
     codigo_barras?: string
 
-    @Column({ type: "timestamp without time zone"})
+    @Column({ type: "timestamp without time zone", nullable: true})
     data_criacao?: Date
 
-    @Column({ type: "timestamp without time zone"})
+    @Column({ type: "timestamp without time zone", nullable: true})
     data_atualizacao?: Date
 
-    @Column({ type: "character varying", length: 50})
+    @Column({ type: "character varying", length: 50, nullable: true})
     marca?: string
 
-    @Column({ type: "boolean"})
+    @Column({ type: "boolean", nullable: true})
     item_consumo?: boolean
 
-    @Column({ type: "boolean"})
+    @Column({ type: "boolean", nullable: true})
     item_venda?: boolean
 
-    @Column({ type: "character varying", length: 20})
+    @Column({ type: "character varying", length: 20, nullable: true})
     status?: string
-}
+};
--- MyProject/src/entity/PurchaseOrder.ts
+++ MyProject/src/entity/PurchaseOrder.ts
@@ -9,15 +9,15 @@ export class PurchaseOrder {
     @Column({ type: "integer"})
     fornecedor_id!: number
 
-    @Column({ type: "date"})
+    @Column({ type: "date", nullable: true})
     data_pedido?: Date
 
-    @Column({ type: "date"})
+    @Column({ type: "date", nullable: true})
     data_entrega?: Date
 
-    @Column({ type: "character varying", length: 20})
+    @Column({ type: "character varying", length: 20, nullable: true})
     status?: string
 
-    @Column({ type: "text"})
+    @Column({ type: "text", nullable: true})
     observacoes?: string
-}
+};
