df7b35d89cbf357306762e499c908a05d5f3dc70
Gabriel <gabriel.forcellini@hotmail.com>
2023-05-20 13:08:25 -0300

    criado rota findone usuarios

--- MyProject/src/index.ts
+++ MyProject/src/index.ts
@@ -14,17 +14,6 @@ const { api_port } = process.env;
 AppDataSource.initialize().then(async () => {
 
     console.log("Conectado ao banco");
-    // const user = new User()
-    // user.nome = "Timber"
-    // user.sobrenome = "Saw"
-    // await AppDataSource.manager.save(user)
-    // console.log("Saved a new user with id: " + user.id)
-
-    // console.log("Loading users from the database...")
-    // const users = await AppDataSource.manager.find(User)
-    // console.log("Loaded users: ", users)
-
-    // console.log("Here you can setup and run express / fastify / any other framework.")
 
     app.use(
         express.urlencoded({ extended: true })
