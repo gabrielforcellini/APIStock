e8dafbe992cd6266171bc9b2c677e611a1ad295f
Gabriel Pavan <gabrielpavan2014@outlook.com>
2023-06-10 11:17:04 -0300

    Alterando category controller

--- MyProject/src/controllers/AddressController.ts
+++ MyProject/src/controllers/AddressController.ts
@@ -11,13 +11,13 @@ export class AddressController {
   static async findAll(req: Request, res: Response) {
     try {
       const addressRespository = AppDataSource
-                                    .getRepository(Address)
-                                    .createQueryBuilder("address")
-                                    .leftJoinAndSelect("address.district" , "district")
-                                    .leftJoinAndSelect("district.city", "city")
-                                    .leftJoinAndSelect("city.state", "state")
-                                    .leftJoinAndSelect("state.country", "country")
-                                    .getMany();
+        .getRepository(Address)
+        .createQueryBuilder("address")
+        .leftJoinAndSelect("address.district", "district")
+        .leftJoinAndSelect("district.city", "city")
+        .leftJoinAndSelect("city.state", "state")
+        .leftJoinAndSelect("state.country", "country")
+        .getMany();
       const address = await addressRespository;
       res.status(200).json({ address, success: true });
     } catch (error) {
@@ -27,9 +27,9 @@ export class AddressController {
   static async findAllContries(req: Request, res: Response) {
     try {
       const contriesRespository = AppDataSource
-                                    .getRepository(Country)
-                                    .createQueryBuilder("country")
-                                    .getMany();
+        .getRepository(Country)
+        .createQueryBuilder("country")
+        .getMany();
       const countries = await contriesRespository;
       res.status(200).json({ countries, success: true });
     } catch (error) {
@@ -39,9 +39,9 @@ export class AddressController {
   static async findAllStates(_req: Request, res: Response) {
     try {
       const statesRespository = AppDataSource
-                                  .getRepository(State)
-                                  .createQueryBuilder("state")
-                                  .getMany();
+        .getRepository(State)
+        .createQueryBuilder("state")
+        .getMany();
       const states = await statesRespository;
       res.status(200).json({ states, success: true });
     } catch (error) {
@@ -51,9 +51,9 @@ export class AddressController {
   static async findAllCities(req: Request, res: Response) {
     try {
       const citiesRespository = AppDataSource
-                                  .getRepository(City)
-                                  .createQueryBuilder("city")
-                                  .getMany();
+        .getRepository(City)
+        .createQueryBuilder("city")
+        .getMany();
       const cities = await citiesRespository;
       res.status(200).json({ cities, success: true });
     } catch (error) {
@@ -63,9 +63,9 @@ export class AddressController {
   static async findAllDistricties(req: Request, res: Response) {
     try {
       const districtiesRespository = AppDataSource
-                                  .getRepository(District)
-                                  .createQueryBuilder("district")
-                                  .getMany();
+        .getRepository(District)
+        .createQueryBuilder("district")
+        .getMany();
       const districties = await districtiesRespository;
       res.status(200).json({ districties, success: true });
     } catch (error) {
@@ -73,26 +73,26 @@ export class AddressController {
     };
   };
 
-  static async findById(req: Request, res: Response){
+  static async findById(req: Request, res: Response) {
     const id = req.params.id;
 
     try {
       const addressRespository = AppDataSource
-                                    .getRepository(Address)
-                                    .createQueryBuilder("address")
-                                    .leftJoinAndSelect("address.district" , "district")
-                                    .leftJoinAndSelect("district.city", "city")
-                                    .leftJoinAndSelect("city.state", "state")
-                                    .leftJoinAndSelect("state.country", "country")
-                                    .where('address.id = :id', { id: parseInt(id)})
-                                    .getOne();
+        .getRepository(Address)
+        .createQueryBuilder("address")
+        .leftJoinAndSelect("address.district", "district")
+        .leftJoinAndSelect("district.city", "city")
+        .leftJoinAndSelect("city.state", "state")
+        .leftJoinAndSelect("state.country", "country")
+        .where('address.id = :id', { id: parseInt(id) })
+        .getOne();
       const address = await addressRespository;
       res.status(200).json({ address, success: true });
     } catch (error) {
       res.status(500).json({ error, success: false });
     };
   }
-  static async findCountryById(req: Request, res: Response){
+  static async findCountryById(req: Request, res: Response) {
     const id = req.params.id;
 
     try {
@@ -103,7 +103,7 @@ export class AddressController {
       res.status(500).json({ error, success: false });
     };
   }
-  static async findStateById(req: Request, res: Response){
+  static async findStateById(req: Request, res: Response) {
     const id = req.params.id;
 
     try {
@@ -114,7 +114,7 @@ export class AddressController {
       res.status(500).json({ error, success: false });
     };
   }
-  static async findCityById(req: Request, res: Response){
+  static async findCityById(req: Request, res: Response) {
     const id = req.params.id;
 
     try {
@@ -125,7 +125,7 @@ export class AddressController {
       res.status(500).json({ error, success: false });
     };
   }
-  static async findDistrictById(req: Request, res: Response){
+  static async findDistrictById(req: Request, res: Response) {
     const id = req.params.id;
 
     try {
@@ -137,7 +137,7 @@ export class AddressController {
     };
   }
 
-  static async createAddress(req: Request, res: Response){
+  static async createAddress(req: Request, res: Response) {
     const {
       street,
       number,
@@ -149,36 +149,36 @@ export class AddressController {
       country_name,
       country_initials
     } = req.body
-    
+
     let country: Country;
     try {
       const countryRepository = AppDataSource.getRepository(Country);
-      country = await countryRepository.findOneBy({ name: country_name});
-    } catch (error){
+      country = await countryRepository.findOneBy({ name: country_name });
+    } catch (error) {
       res.status(500).json({ error, success: false });
     }
-      
-    if(!country){
+
+    if (!country) {
       country = new Country();
       country.name = country_name;
       country.initials = country_initials;
-      
+
       country = await AppDataSource.manager.save(country);
     }
     let state: State;
     try {
       const stateRepository = AppDataSource.getRepository(State)
-                                              .createQueryBuilder("state")
-                                              .leftJoin("state.country", 'country')
-                                              .where("state.country = :id", { id: country.id})
-                                              .andWhere("state.name = :name", { name: state_name})
-                                              .getOne();
+        .createQueryBuilder("state")
+        .leftJoin("state.country", 'country')
+        .where("state.country = :id", { id: country.id })
+        .andWhere("state.name = :name", { name: state_name })
+        .getOne();
       state = await stateRepository;
     } catch (error) {
       res.status(500).json({ error, success: false });
     }
-  
-    if(!state){
+
+    if (!state) {
       state = new State();
       state.name = state_name;
       state.initials = state_initials;
@@ -190,44 +190,44 @@ export class AddressController {
     let city: City;
     try {
       const cityRepository = AppDataSource.getRepository(City)
-                                            .createQueryBuilder("city")
-                                            .leftJoin("city.state", 'state')
-                                            .where("city.state = :id", { id: state.id})
-                                            .andWhere("city.name = :name", { name: city_name})
-                                            .getOne();
+        .createQueryBuilder("city")
+        .leftJoin("city.state", 'state')
+        .where("city.state = :id", { id: state.id })
+        .andWhere("city.name = :name", { name: city_name })
+        .getOne();
       city = await cityRepository;
-    } catch (error){
+    } catch (error) {
       res.status(500).json({ error, success: false });
     }
-    
-    if(!city){
+
+    if (!city) {
       city = new City();
       city.name = city_name;
       city.state = state;
-    
+
       city = await AppDataSource.manager.save(city);
     }
 
     let district: District;
     try {
       const districtRepository = AppDataSource.getRepository(District)
-                                                .createQueryBuilder("district")
-                                                .leftJoin("district.city", 'city')
-                                                .where("district.city = :id", { id: city.id})
-                                                .andWhere("district.name = :name", { name: district_name})
-                                                .getOne();
+        .createQueryBuilder("district")
+        .leftJoin("district.city", 'city')
+        .where("district.city = :id", { id: city.id })
+        .andWhere("district.name = :name", { name: district_name })
+        .getOne();
       district = await districtRepository;
     } catch (error) {
       res.status(500).json({ error, success: false });
     }
 
-    if(!district){
+    if (!district) {
       district = new District();
       district.name = district_name;
       district.city = city;
 
       district = await AppDataSource.manager.save(district);
-    } 
+    }
 
     const address = new Address();
     address.street = street;
@@ -235,11 +235,11 @@ export class AddressController {
     address.zip_code = zip_code;
     address.district = district;
 
-    try{
+    try {
       const newAddress = await AppDataSource.manager.save(address);
-      res.status(200).json({ id: newAddress.id, success: true})
-    } catch (error){
+      res.status(200).json({ id: newAddress.id, success: true })
+    } catch (error) {
       res.status(500).json({ error, success: false });
-    };  
+    };
   }
 };
--- MyProject/src/controllers/CategoryController.ts
+++ MyProject/src/controllers/CategoryController.ts
@@ -3,92 +3,92 @@ import { Category } from '../entity/Category';
 import { AppDataSource } from '../data-source';
 
 export class CategoryController {
-  /**
-   * Register an category
-   */
-  static async register(req: Request, res: Response) {
-    const {
-      code,
-      description
-    } = req.body;
 
-    const category = new Category();
-    category.code = code;
-    category.description = description;
+  static async register(req: Request, res: Response) {
+    const { code, description } = req.body;
 
     try {
-      const newCategory = await AppDataSource.manager.save(category);
+      const categoryRepository = AppDataSource.getRepository(Category)
+      const newCategory = await categoryRepository
+        .createQueryBuilder("category")
+        .insert()
+        .into("category")
+        .values(
+          { code: code, description: description }
+        )
+        .execute();
+
       res.status(200).json({ newCategory, success: true });
     } catch (error) {
       res.status(500).json({ error, success: false });
     };
   };
 
-  /**
-   * Find a category by id
-   */
   static async findOneById(req: Request, res: Response) {
     const id = req.params.id;
 
     try {
       const categoryRepository = AppDataSource.getRepository(Category);
-      const category = await categoryRepository.findOneBy({ id: parseInt(id) });
+      const category = await categoryRepository
+        .createQueryBuilder("category")
+        .select("category")
+        .where("id = :id", { id: id })
+        .getOne();
+
       res.status(200).json({ category, success: true });
     } catch (error) {
       res.status(500).json({ error, success: false });
     };
   };
 
-  /**
-   * Find all categories from database
-   */
   static async findAll(req: Request, res: Response) {
     try {
       const categoryRespository = AppDataSource.getRepository(Category);
-      const categories = await categoryRespository.find();
+      const categories = await categoryRespository
+        .createQueryBuilder("category")
+        .select("category")
+        .getMany();
+
       res.status(200).json({ categories, success: true });
     } catch (error) {
       res.status(500).json({ error, success: false });
     };
   };
 
-  /**
-   * Update an category by id
-   */
   static async updateOne(req: Request, res: Response) {
     const id = req.params.id;
 
-    const {
-      code,
-      description
-    } = req.body;
+    const { code, description } = req.body;
 
     try {
       const categoryRepository = AppDataSource.getRepository(Category);
-      const categoryToUpdate = await categoryRepository.findOneBy({
-        id: parseInt(id)
-      });
-      categoryToUpdate.code = code;
-      categoryToUpdate.description = description;
-      await categoryRepository.save(categoryToUpdate);
+      const categoryToUpdate = await categoryRepository
+        .createQueryBuilder("category")
+        .update("category")
+        .set(
+          { code: code, description: description }
+        )
+        .where("id = :id", { id: id })
+        .execute();
+
       res.status(200).json({ categoryToUpdate, success: true });
     } catch (error) {
       res.status(500).json({ error, success: false });
     }
   };
 
-  /**
-   * Delete an category by id
-   */
   static async delete(req: Request, res: Response) {
     const id = req.params.id;
 
     try {
       const categoryRepository = AppDataSource.getRepository(Category);
-      const categoryToUpdate = await categoryRepository.findOneBy({
-        id: parseInt(id)
-      });
-      await categoryRepository.remove(categoryToUpdate);
+      const categoryToUpdate = await categoryRepository
+        .createQueryBuilder("category")
+        .delete()
+        .from("category")
+        .where("id = :id", { id: id })
+        .execute();
+
       res.status(200).json({ success: true });
     } catch (error) {
       res.status(500).json({ error, success: false });
